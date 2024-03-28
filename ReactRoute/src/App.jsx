d59767ee210a2a0c0e7769ffaee808a67811013d
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-28 20:36:04 +0530

    commit

--- ReactRoute/src/App.jsx
+++ ReactRoute/src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Route,Routes} from 'react-router-dom';
+import NotFound from './Components/NotFound';
 
 function App() {
   
 
   return (
-    <>
-      App
+    <div className = "App">
+      <Routes>
+        <Route path = "/" element ={<div>Home Page</div>}  />
+        <Route path = "/support" element ={<Home/>}  />
+        <Route path = "/about" element ={<About/>}  />
+        <Route path = "/labs" element ={<Labs/>}  />
+        <Route path = "*" element ={<NotFound/>}  />
         
-    </>
+      </Routes>
+     
+        
+    </div>
   )
 }
 
--- ReactRoute/src/main.jsx
+++ ReactRoute/src/main.jsx
@@ -4,7 +4,10 @@ import App from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>
  
-    <App />
+ 
  
 )
