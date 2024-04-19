cedd3a5b99990a8e14999f35a551941e9a26447c
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-19 10:31:16 +0530

    navigate

--- ReactRoute/src/Components/Labs.jsx
+++ ReactRoute/src/Components/Labs.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-const Labs = () =>{
-    return (
+import { useNavigate } from "react-router-dom";
+const Labs = () => {
+    const navigate = useNavigate();
+    function clickHandler() {
+        // move to about page
+        navigate("/about");
+
+    }
+    return (<div>
         <div>
-This is lab page
-        
+            This is lab page
         </div>
-            )
+        <button onClick={clickHandler}>Move to About page</button>
+
+
+
+    </div>
+
+
+    )
 }
 export default Labs
