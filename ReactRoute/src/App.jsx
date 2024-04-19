d95e1b12cb63226251d597b88dc5754040fac304
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-19 09:38:43 +0530

    commit

--- ReactRoute/src/App.jsx
+++ ReactRoute/src/App.jsx
@@ -1,37 +1,37 @@
 import './App.css'
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from "./Components/Home";
 import About from './Components/About';
 import Labs from './Components/Labs';
 import NotFound from './Components/NotFound';
 import Support from './Components/Support';
-import {Link, NavLink } from 'react-router-dom';   
+import { Link, NavLink } from 'react-router-dom';
 
 function App() {
-  
 
-// routes is used to navigate between pages and navigations me page reload nhi hona chahiyeee dynamically usi page par changes hote hai
+
+  // routes is used to navigate between pages and navigations me page reload nhi hona chahiyeee dynamically usi page par changes hote hai
 
   return (
-    <div className = "App">
+    <div className="App">
       <nav>
         <ul>
-          <li><NavLink to ="/">Home</NavLink></li>
-          <li><NavLink to = "/about">About</NavLink></li>
-          <li><NavLink to = "/support">Support</NavLink></li>
-          <li><NavLink to = "/labs">Labs</NavLink></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/support">Support</NavLink></li>
+          <li><NavLink to="/labs">Labs</NavLink></li>
         </ul>
       </nav>
       <Routes>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/support" element ={<Support/>}  />
-        <Route path = "/about" element ={<About/>}  />
-        <Route path = "/labs" element ={<Labs/>}  />
-        <Route path = "*" element ={<NotFound/>}  />
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/labs" element={<Labs />} />
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
-     
-        
+
+
     </div>
   );
 }
