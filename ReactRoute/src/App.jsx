7f5c15797494ad4b5110fa6c8bd3ed0cd3e01289
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-19 09:19:20 +0530

    commit

--- ReactRoute/src/App.jsx
+++ ReactRoute/src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {Route,Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Home from "./Components/Home";
 import About from './Components/About';
 import Labs from './Components/Labs';
 import NotFound from './Components/NotFound';
 import Support from './Components/Support';
-import {Link } from 'react-router-dom';   
+import {Link, NavLink } from 'react-router-dom';   
 
 function App() {
   
+
+// routes is used to navigate between pages and navigations me page reload nhi hona chahiyeee dynamically usi page par changes hote hai
 
   return (
     <div className = "App">
       <nav>
         <ul>
-          <li><Link to ="/">Home</Link></li>
-          <li>
-            <Link to = "/about">About</Link></li>
-          <li><Link to = "/support">Support</Link></li>
-          <li><Link to = "/labs"></Link>Labs</li>
+          <li><NavLink to ="/">Home</NavLink></li>
+          <li><NavLink to = "/about">About</NavLink></li>
+          <li><NavLink to = "/support">Support</NavLink></li>
+          <li><NavLink to = "/labs"></NavLink>Labs</li>
         </ul>
       </nav>
       <Routes>
-        < Route path = "/" element = {<Home/>}/>
+        <Route path = "/" element = {<Home/>}/>
         <Route path = "/support" element ={<Support/>}  />
         <Route path = "/about" element ={<About/>}  />
         <Route path = "/labs" element ={<Labs/>}  />
--- ReactRoute/src/main.jsx
+++ ReactRoute/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // different different path ko link karna browser ke sath hii browserrouter ka kaam h
   <BrowserRouter>
      <App />
   </BrowserRouter>
